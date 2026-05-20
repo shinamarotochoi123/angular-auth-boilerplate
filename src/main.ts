60d1a2447c389be824ee6b46bdea5722dafc1cf1
shinamarotochoi123 <beomchoi123@gmail.com>
2026-05-21 01:17:05 +0800

    Fix main.ts merge conflicts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-=======
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModule } from './app/app-module';
-
-platformBrowser().bootstrapModule(AppModule, {
-  
-})
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
->>>>>>> fe43366e5d7dea55629455c1572c933d76e23f52
